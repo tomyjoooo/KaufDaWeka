68b547fec8a944aa81505169a757cffefb148167
tomyjoooo <tomwessels@web.de>
2018-04-12 16:37:28 +0200

    Korrektur Projektstruktur

--- src/main/java/Moin.java
+++ src/main/java/Moin.java
@@ -25,7 +25,7 @@ public class Moin extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("{ data : 'Daten', !!!HIER JSON DATEN!! }");
 	}
 
 	/**
@@ -34,6 +34,7 @@ public class Moin extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		//HIER DATEI ANNEHMEN UND AN WEKA WEITERGEBEN
 	}
 
 }
